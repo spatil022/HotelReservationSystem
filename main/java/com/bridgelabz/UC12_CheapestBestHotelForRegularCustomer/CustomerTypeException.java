2027c8896a0831b7bcb784948af5659edd04729b
spatil022 <shamalpatil1998@gmail.com>
2021-02-26 21:16:18 +0530

    added

---
+++ main/java/com/bridgelabz/UC12_CheapestBestHotelForRegularCustomer/CustomerTypeException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.UC12_CheapestBestHotelForRegularCustomer;
+
+public class CustomerTypeException extends Exception {
+	public CustomerTypeException(String message) {
+		super(message);
+	}
+}
---
+++ main/java/com/bridgelabz/UC12_CheapestBestHotelForRegularCustomer/HotelDetails.java
@@ -0,0 +1,71 @@
+package com.bridgelabz.UC12_CheapestBestHotelForRegularCustomer;
+
+public class HotelDetails {
+
+	private String hotelName;
+	private int weekdayRoomRateRegular;
+	private int weekendRoomRateRegular;
+	private int weekdayRoomRateRewards;
+	private int weekendRoomRateRewards;
+	private int rating;
+
+	public HotelDetails(String name, int weekdayRoomRateRegular, int weekendRoomRateRegular, int weekdayRoomRateRewards, int weekendRoomRateRewards, int rating) {
+		this.hotelName = name;
+		this.weekdayRoomRateRegular = weekdayRoomRateRegular;
+		this.weekendRoomRateRegular = weekendRoomRateRegular;
+		this.weekdayRoomRateRewards = weekdayRoomRateRewards;
+		this.weekendRoomRateRewards = weekendRoomRateRewards;
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return hotelName;
+	}
+	public void setName(String name) {
+		this.hotelName = name;
+	}
+	public int getWeekdayRoomRateRegular() {
+		return weekdayRoomRateRegular;
+	}
+
+	public void setWeekdayRoomRateRegular(int weekdayRoomRateRegular) {
+		this.weekdayRoomRateRegular = weekdayRoomRateRegular;
+	}
+
+	public int getWeekendRoomRateRegular() {
+		return weekendRoomRateRegular;
+	}
+
+	public void setWeekendRoomRateRegular(int weekendRoomRateReuglar) {
+		this.weekendRoomRateRegular = weekendRoomRateReuglar;
+	}
+	public int getWeekdayRoomRateRewards() {
+		return weekdayRoomRateRewards;
+	}
+
+	public void setWeekdayRoomRateRewards(int weekdayRoomRateRewards) {
+		this.weekdayRoomRateRewards = weekdayRoomRateRewards;
+	}
+
+	public int getWeekendRoomRateRewards() {
+		return weekendRoomRateRewards;
+	}
+
+	public void setWeekendRoomRateRewards(int weekendRoomRateRewards) {
+		this.weekendRoomRateRewards = weekendRoomRateRewards;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString(){
+		return "HotelDetails [hotelName=" + hotelName + ", weekdayRoomRate=" + weekdayRoomRateRegular + ", weekendRoomRate="
+				+ weekendRoomRateRegular + ", rating=" + rating + "]";
+	}
+}
