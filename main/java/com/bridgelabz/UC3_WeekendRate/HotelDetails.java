86b910017590a4859106634cf70a0e4d3c9426eb
spatil022 <shamalpatil1998@gmail.com>
2021-02-26 20:43:13 +0530

    added

---
+++ main/java/com/bridgelabz/UC3_WeekendRate/HotelDetails.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.UC3_WeekendRate;
+
+public class HotelDetails {
+
+	private String name;
+	private int weekdayRoomRate;
+	private int weekendRoomRate;
+
+	public HotelDetails(String name, int weekdayRoomRate, int weekendRoomRate) {
+		this.name = name;
+		this.weekdayRoomRate = weekdayRoomRate;
+		this.weekendRoomRate = weekendRoomRate;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeekdayRoomRate() {
+		return weekdayRoomRate;
+	}
+
+	public void setWeekdayRoomRate(int weekdayRoomRate) {
+		this.weekdayRoomRate = weekdayRoomRate;
+	}
+
+	public int getWeekendRoomRate() {
+		return weekendRoomRate;
+	}
+
+	public void setWeekendRoomRate(int weekendRoomRate) {
+		this.weekendRoomRate = weekendRoomRate;
+	}
+
+	@Override
+	public String toString(){
+		return "HotelDetails [hotelName=" + name + ", weekdayRoomRate=" + weekdayRoomRate + ", weekendRoomRate="
+				+ weekendRoomRate + "]";
+	}
+}
